c58f3ea98efc501ac4a5c1275f69405587bdb502
mohitsethi <mohit@sethis.in>
2021-08-13 12:31:09 +0530

    updated kafka code

--- kafka/src/main/java/ConsumerApp.java
+++ kafka/src/main/java/ConsumerApp.java
@@ -8,36 +8,38 @@ public class ConsumerApp {
 
         // Create the Properties class to instantiate the Consumer with the desired settings:
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092, localhost:9093");
+//        props.put("bootstrap.servers", "localhost:9092, localhost:9093");
+        props.put("bootstrap.servers", "rmb.Dlink:9092, rmb.Dlink:7092");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("fetch.min.bytes", 1);
-        props.put("group.id", "");
-        props.put("heartbeat.interval.ms", 3000);
-        props.put("max.partition.fetch.bytes", 1048576);
-        props.put("session.timeout.ms", 30000);
-        props.put("auto.offset.reset", "latest");
-        props.put("connections.max.idle.ms", 540000);
+//        props.put("fetch.min.bytes", 1);
+//        props.put("group.id", "");
+//        props.put("heartbeat.interval.ms", 3000);
+//        props.put("max.partition.fetch.bytes", 1048576);
+//        props.put("session.timeout.ms", 30000);
+//        props.put("auto.offset.reset", "latest");
+//        props.put("connections.max.idle.ms", 540000);
         props.put("enable.auto.commit", true);
-        props.put("exclude.internal.topics", true);
+//        props.put("exclude.internal.topics", true);
         props.put("max.poll.records", 2147483647);
-        props.put("partition.assignment.strategy", "org.apache.kafka.clients.consumer.RangeAssignor");
-        props.put("request.timeout.ms", 40000);
-        props.put("auto.commit.interval.ms", 5000);
-        props.put("fetch.max.wait.ms", 500);
-        props.put("metadata.max.age.ms", 300000);
-        props.put("reconnect.backoff.ms", 50);
-        props.put("retry.backoff.ms", 100);
-        props.put("client.id", "");
+//        props.put("partition.assignment.strategy", "org.apache.kafka.clients.consumer.RangeAssignor");
+//        props.put("request.timeout.ms", 40000);
+//        props.put("auto.commit.interval.ms", 5000);
+//        props.put("fetch.max.wait.ms", 500);
+//        props.put("metadata.max.age.ms", 300000);
+//        props.put("reconnect.backoff.ms", 50);
+//        props.put("retry.backoff.ms", 100);
+//        props.put("client.id", "");
 
 
+        String topic = "ms_topic";
         // Create a KafkaConsumer instance and configure it with properties.
         KafkaConsumer<String, String> myConsumer = new KafkaConsumer<String, String>(props);
 
         // Create a topic subscription list:
         ArrayList<TopicPartition> partitions = new ArrayList<TopicPartition>();
-        partitions.add(new TopicPartition("my-topic", 0)); // Adds a TopicPartition instance representing a topic and a partition.
-        partitions.add(new TopicPartition("my-topic", 2)); // Adds an additional TopicPartition instance representing a different partition within the topic. Change as desired.
+        partitions.add(new TopicPartition("ms_topic", 0)); // Adds a TopicPartition instance representing a topic and a partition.
+//        partitions.add(new TopicPartition("ms_topic", 2)); // Adds an additional TopicPartition instance representing a different partition within the topic. Change as desired.
         // Assign partitions to the Consumer:
         myConsumer.assign(partitions);
 
@@ -45,7 +47,7 @@ public class ConsumerApp {
         Set<TopicPartition> assignedPartitions = myConsumer.assignment();
 
         // Print the partition assignments:
-        printSet(assignedPartitions);
+//        printSet(assignedPartitions);
 
         // Start polling for messages:
         try {
