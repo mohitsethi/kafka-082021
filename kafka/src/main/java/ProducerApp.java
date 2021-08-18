7bf068204026c76e4f1deb742549b3160d993a3e
mohitsethi <mohit@sethis.in>
2021-08-18 11:02:05 +0530

    updated kafka code

--- kafka/src/main/java/ProducerApp.java
+++ kafka/src/main/java/ProducerApp.java
@@ -10,23 +10,43 @@ public class ProducerApp {
         // Create the Properties class to instantiate the Consumer with the desired settings:
         Properties props = new Properties();
 //        props.put("bootstrap.servers", "localhost:9092, localhost:9093");
-        props.put("bootstrap.servers", "rmb.Dlink:9092, rmb.Dlink:7092");
+//        props.put("bootstrap.servers", "rmb.Dlink:9092, rmb.Dlink:7092");
+        props.put("bootstrap.servers", "ec2-34-208-1-180.us-west-2.compute.amazonaws.com:9092");
+//        34.208.1.180
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        // props.put("acks", "");
-        // props.put("buffer.memory", 33554432);
-        // props.put("compression.type", "none");
-        // props.put("retries", 0);
-        // props.put("batch.size", 16384);
-        // props.put("client.id", "");
-        // props.put("linger.ms", 0);
-        // props.put("max.block.ms", 60000);
-        // props.put("max.request.size", 1048576);
-        // props.put("partitioner.class", "org.apache.kafka.clients.producer.internals.DefaultPartitioner");
-        // props.put("request.timeout.ms", 30000);
-        // props.put("timeout.ms", 30000);
-        // props.put("max.in.flight.requests.per.connection", 5);
-        // props.put("retry.backoff.ms", 5);
+
+
+        // Record Accumulator
+        props.put("batch.size", 16384);
+        props.put("buffer.memory", 33554432);
+        props.put("linger.ms", 0);
+        props.put("max.block.ms", 60000);
+//         props.put("compression.type", "none");
+
+        // Delivery Guarantees
+        props.put("acks", "");
+        props.put("retries", 0);
+        props.put("retry.backoff.ms", 5);
+
+        // Order Guarantees
+        props.put("max.in.flight.requests.per.connection", 5);
+        props.put("retry.backoff.ms", 5);
+
+        // Challenge#1: 2 partitions, every message needs to go in all partitions.
+            // producer -> msg1, msg2
+            // consumer -> msg1, msg2, msg2, msg1
+            // create topic with >1 partition
+        // Challenge#2: guarantee order, create two producers
+           // consumer -> all in LogTimeAppend
+
+//        props.put("client.id", "");
+//        props.put("max.request.size", 1048576);
+//        props.put("partitioner.class", "org.apache.kafka.clients.producer.internals.DefaultPartitioner");
+//        props.put("request.timeout.ms", 30000);
+//        props.put("timeout.ms", 30000);
+
+
 
         KafkaProducer<String, String> myProducer = new KafkaProducer<String, String>(props);
         DateFormat dtFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS");
